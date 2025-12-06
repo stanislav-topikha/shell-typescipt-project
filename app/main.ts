2712407a116b4ca4399e23007bf545d3eae13b45
stanislav-t <stanislav.topikha@gmail.com>
2025-12-05 22:34:19 -0500

    type extended

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
-import { createInterface } from "readline";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { createInterface } from "node:readline";
+
 
 const rl = createInterface({
   input: process.stdin,
@@ -39,6 +42,38 @@ function normalizeCommand(rawInput: string) {
   }
 };
 
+function locateExecutableFile(xFileName: string){
+  const pathArr = process.env.PATH?.split(path.delimiter) || [];
+  let result: string | null = null;
+
+  for (const tmpPath of pathArr) {
+    if (result) {
+      break;
+    }
+
+    try {
+      const files = fs.readdirSync(path.join(tmpPath), {withFileTypes: true});
+
+      for (const file of files) {
+        if(file.name === xFileName) {
+          const filePath = path.join(tmpPath, file.name);
+
+          try {
+            fs.accessSync(filePath, fs.constants.X_OK);
+            result = `${file.name} is ${filePath}`;
+          } catch {}
+
+          break;
+        }
+      }
+    } catch {}
+  }
+
+  return result;
+}
+
+console.log(locateExecutableFile('pfctl'));
+
 function REPL() {
   rl.question("$ ", function (input) {
     const command = normalizeCommand(input);
@@ -54,13 +89,23 @@ function REPL() {
 
       case(CommandAction.Type):
         {
-          const tmpCommand = command.rest ||'_error_';
+          const tmpCommand = command.rest;
 
-          console.log(
-            normalizeCommand(tmpCommand).action !== CommandAction.Unknown
-              ? `${tmpCommand} is a shell builtin`
-              : `${tmpCommand} not found`
-          );
+          if (!tmpCommand) {
+            break;
+          }
+
+          let typeResult: null | string = null;
+
+          if (normalizeCommand(tmpCommand).action !== CommandAction.Unknown) {
+            typeResult = `${tmpCommand} is a shell builtin`;
+          }
+
+          if (!typeResult) {
+            typeResult = locateExecutableFile(tmpCommand);
+          }
+
+          console.log(typeResult ?? `${tmpCommand} not found`);
         }
         break;
 
