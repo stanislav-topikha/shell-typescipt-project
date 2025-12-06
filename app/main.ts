d09b459121c0e62434f9d593da9013059675e62b
stanislav-t <stanislav.topikha@gmail.com>
2025-12-05 20:21:44 -0500

    command: type

--- app/main.ts
+++ app/main.ts
@@ -9,39 +9,59 @@ enum CommandAction {
   Exit = 'exit',
   Unknown = 'unknown',
   Echo = 'echo',
+  Type = 'type',
 }
+
+function normalizeCommand(rawInput: string) {
+  const rawInputWords = rawInput.split(' ');
+  const firstWord = rawInputWords[0];
+  const restOfInput = rawInputWords.slice(1).join(' ');
+
+  switch (firstWord) {
+    case (CommandAction.Exit):
+      return {
+        action: CommandAction.Exit,
+      };
+    case (CommandAction.Echo):
+      return {
+        action: CommandAction.Echo,
+        rest: restOfInput,
+      }
+    case (CommandAction.Type):
+      return {
+        action: CommandAction.Type,
+        rest: restOfInput,
+      }
+    default:
+      return {
+        action: CommandAction.Unknown,
+      }
+  }
+};
 
 function REPL() {
   rl.question("$ ", function (input) {
-    const normalizeCommand = ((rawInput) => {
-      const rawInputWords = rawInput.split(' ');
-      const firstWord = rawInputWords[0];
-      const restOfInput = rawInputWords.slice(1).join(' ');
+    const command = normalizeCommand(input);
 
-      switch(firstWord) {
-        case(CommandAction.Exit):
-          return {
-            action: CommandAction.Exit,
-          };
-        case(CommandAction.Echo):
-          return {
-            action: CommandAction.Echo,
-            rest: restOfInput,
-          }
-        default:
-          return {
-            action: CommandAction.Unknown,
-          }
-      }
-    })(input);
-
-    switch(normalizeCommand.action) {
+    switch(command.action) {
       case(CommandAction.Exit):
         rl.close();
         return;
 
       case(CommandAction.Echo):
-        console.log(`${normalizeCommand.rest}`);
+        console.log(`${command.rest}`);
+        break;
+
+      case(CommandAction.Type):
+        {
+          const tmpCommand = command.rest ||'_error_';
+
+          console.log(
+            normalizeCommand(tmpCommand).action !== CommandAction.Unknown
+              ? `${tmpCommand}: is a shell builtin`
+              : `${tmpCommand}: not found`
+          );
+        }
         break;
 
       case(CommandAction.Unknown):
