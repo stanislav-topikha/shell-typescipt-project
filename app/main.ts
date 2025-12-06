596e78b0573cf3ba4ec499d33b2de4098ed3b14d
stanislav-t <stanislav.topikha@gmail.com>
2025-12-05 19:12:41 -0500

    command: exit

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,19 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+enum Command {
+  Exit = 'exit',
+}
 
 function  REPL() {
   rl.question("$ ", function (command) {
+    if (command === Command.Exit) {
+      rl.close();
+      return;
+    }
+
     console.log("".concat(command, ": command not found"));
+
     REPL();
   });
 };
