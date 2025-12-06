7919436d9bb8a3bc0766d6d504b294848e89db06
stanislav-t <stanislav.topikha@gmail.com>
2025-12-05 19:56:19 -0500

    command: echo

--- app/main.ts
+++ app/main.ts
@@ -5,18 +5,48 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-enum Command {
+enum CommandAction {
   Exit = 'exit',
+  Unknown = 'unknown',
+  Echo = 'echo',
 }
 
-function  REPL() {
-  rl.question("$ ", function (command) {
-    if (command === Command.Exit) {
-      rl.close();
-      return;
-    }
+function REPL() {
+  rl.question("$ ", function (input) {
+    const normalizeCommand = ((rawInput) => {
+      const rawInputWords = rawInput.split(' ');
+      const firstWord = rawInputWords[0];
+      const restOfInput = rawInputWords.slice(1).join(' ');
 
-    console.log("".concat(command, ": command not found"));
+      switch(firstWord) {
+        case(CommandAction.Exit):
+          return {
+            action: CommandAction.Exit,
+          };
+        case(CommandAction.Echo):
+          return {
+            action: CommandAction.Echo,
+            rest: restOfInput,
+          }
+        default:
+          return {
+            action: CommandAction.Unknown,
+          }
+      }
+    })(input);
+
+    switch(normalizeCommand.action) {
+      case(CommandAction.Exit):
+        rl.close();
+        return;
+
+      case(CommandAction.Echo):
+        console.log(`${normalizeCommand.rest}`);
+        break;
+
+      case(CommandAction.Unknown):
+        console.log("".concat(input, ": command not found"));
+    }
 
     REPL();
   });
