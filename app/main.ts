69031a8fa889391d0e3b547a60e82650f03fbcc7
stanislav-t <stanislav.topikha@gmail.com>
2025-12-11 16:54:27 -0500

    regexp fix

--- app/main.ts
+++ app/main.ts
@@ -95,7 +95,7 @@ function processCommand(input: string) {
     }
 
     case (COMMAND_ACTION.Echo): {
-      consoleOutput = `${command.leftoverWords.join('')}`;
+      consoleOutput = `${command.leftoverWords.join(' ')}`;
       break;
     }
 
